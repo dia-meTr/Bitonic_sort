203176896f33130340dae48d30ab96b8797f45e1
dia-meTr <diana.tryhub@gmail.com>
2023-05-11 15:51:32 +0300

    radomizer

--- randomize_list.py
+++ randomize_list.py
@@ -2,8 +2,8 @@ import random
 
 
 with open("data.txt", 'w') as f:
-    for n in [65536, 131072, 262144]:
+    for n in [262144, 131072, 65536]:
         for i in range(0, 3):
-            a = random.sample(range(10, 362144), n)
+            a = random.sample(range(0, 362144), n)
             print(a)
             f.write(str(a) + '\n')
